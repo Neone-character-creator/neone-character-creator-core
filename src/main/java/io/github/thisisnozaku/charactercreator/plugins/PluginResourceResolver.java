f252cd950b4387f01630a30aa80aa38977ca2bb7
Damien <damienmarble@hotmail.com>
2016-01-09 22:48:35 -0800

    Removed PluginResourceResolver.

--- src/main/java/io/github/thisisnozaku/charactercreator/plugins/PluginResourceResolver.java
+++
@@ -1,9 +0,0 @@
-package io.github.thisisnozaku.charactercreator.plugins;
-
-import org.springframework.web.servlet.resource.ResourceResolver;
-
-/**
- * Created by Damien on 1/6/2016.
- */
-public interface PluginResourceResolver extends ResourceResolver {
-}
