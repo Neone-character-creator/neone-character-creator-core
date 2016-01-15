cceb0eb91e99668290f2cf1e9a1f6b8aa4e1e82c
Damien <damienmarble@hotmail.com>
2016-01-15 00:12:25 -0800

    Added no-args constructor and setters for PluginDescription.

--- neone-plugin-api/src/main/java/io/github/thisisnozaku/charactercreator/plugins/PluginDescription.java
+++ neone-plugin-api/src/main/java/io/github/thisisnozaku/charactercreator/plugins/PluginDescription.java
@@ -4,35 +4,50 @@ package io.github.thisisnozaku.charactercreator.plugins;
  * Created by Damien on 11/25/2015.
  */
 public class PluginDescription {
-    private final String authorName;
-    private final String systemName;
-    private final String version;
+    private String author;
+    private String system;
+    private String version;
 
     public String getVersion() {
         return version;
     }
 
-    public String getSystemName() {
-        return systemName;
+    public String getSystem() {
+        return system;
     }
 
-    public String getAuthorName() {
-        return authorName;
+    public String getAuthor() {
+        return author;
     }
 
-    public PluginDescription(String authorName, String systemName, String version) {
-        if (authorName == null) {
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public void setSystem(String system) {
+        this.system = system;
+    }
+
+    public void setVersion(String version) {
+        this.version = version;
+    }
+
+    public PluginDescription(String author, String system, String version) {
+        if (author == null) {
             throw new IllegalStateException("author name cannot be null");
         }
-        if (systemName == null) {
+        if (system == null) {
             throw new IllegalStateException("system name cannot be null");
         }
         if (version == null) {
             throw new IllegalStateException("version cannot be null");
         }
-        this.authorName = authorName;
-        this.systemName = systemName;
+        this.author = author;
+        this.system = system;
         this.version = version;
+    }
+
+    public PluginDescription() {
     }
 
     @Override
@@ -42,16 +57,16 @@ public class PluginDescription {
 
         PluginDescription that = (PluginDescription) o;
 
-        if (!authorName.equals(that.authorName)) return false;
-        if (!systemName.equals(that.systemName)) return false;
+        if (!author.equals(that.author)) return false;
+        if (!system.equals(that.system)) return false;
         return version.equals(that.version);
 
     }
 
     @Override
     public int hashCode() {
-        int result = authorName.hashCode();
-        result = 31 * result + systemName.hashCode();
+        int result = author.hashCode();
+        result = 31 * result + system.hashCode();
         result = 31 * result + version.hashCode();
         return result;
     }
@@ -59,8 +74,8 @@ public class PluginDescription {
     @Override
     public String toString() {
         return "PluginDescription{" +
-                "authorName='" + authorName + '\'' +
-                ", systemName='" + systemName + '\'' +
+                "author='" + author + '\'' +
+                ", system='" + system + '\'' +
                 ", version='" + version + '\'' +
                 '}';
     }
