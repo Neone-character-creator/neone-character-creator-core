6ffb30ee360a9e97371c0af642cf517f3f2d9a69
Damien <damienmarble@hotmail.com>
2016-01-20 00:35:44 -0800

    Added RepositoryDefinition annotation, renamed to UserRepository and changed to make use of User class; was using an extraneous Account class previously.

--- src/main/java/io/github/thisisnozaku/charactercreator/data/UserRepository.java
+++ src/main/java/io/github/thisisnozaku/charactercreator/data/UserRepository.java
@@ -1,15 +1,17 @@
 package io.github.thisisnozaku.charactercreator.data;
 
+import io.github.thisisnozaku.charactercreator.model.User;
 import org.hibernate.validator.internal.engine.resolver.JPATraversableResolver;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.RepositoryDefinition;
 import org.springframework.social.facebook.api.Account;
 import org.springframework.stereotype.Repository;
 
 /**
  * Created by Damien on 11/15/2015.
  */
-@Repository
-public interface AccountRepository extends JpaRepository<Account, Long> {
+@RepositoryDefinition(domainClass = User.class, idClass = Long.class)
+public interface UserRepository extends JpaRepository<User, Long> {
 
 }
