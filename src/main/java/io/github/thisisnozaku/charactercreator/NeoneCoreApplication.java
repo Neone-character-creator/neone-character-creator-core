956d59c075c136aae34996fdb8cb89840d3dfd51
Damien <damienmarble@hotmail.com>
2016-01-20 00:48:06 -0800

    Removed exclusion property from @SpringBootApplication

--- src/main/java/io/github/thisisnozaku/charactercreator/NeoneCoreApplication.java
+++ src/main/java/io/github/thisisnozaku/charactercreator/NeoneCoreApplication.java
@@ -2,10 +2,11 @@ package io.github.thisisnozaku.charactercreator;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 import static org.springframework.boot.SpringApplication.run;
 
-@SpringBootApplication(exclude = {HibernateJpaAutoConfiguration.class})
+@SpringBootApplication
 public class NeoneCoreApplication {
 
     public static void main(String[] args) {
