fa2d3c8cb208dcc112edd1aa2948fde6324858a1
Damien <damienmarble@hotmail.com>
2016-01-29 17:14:08 -0800

    Test class for mongo repository.

--- src/test/java/io/github/thisisnozaku/charactercreator/data/CharacterMongoRepositoryTest.java
+++ src/test/java/io/github/thisisnozaku/charactercreator/data/CharacterMongoRepositoryTest.java
@@ -1,20 +1,12 @@
 package io.github.thisisnozaku.charactercreator.data;
 
-import com.sun.webkit.plugin.PluginManager;
 import io.github.thisisnozaku.charactercreator.NeoneCoreApplication;
-import io.github.thisisnozaku.charactercreator.data.CharacterMongoRepository;
 import io.github.thisisnozaku.charactercreator.plugins.Character;
 import io.github.thisisnozaku.charactercreator.plugins.PluginDescription;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.Mock;
-import org.mockito.Mockito;
-import org.springframework.beans.factory.ListableBeanFactory;
-import org.springframework.beans.factory.support.BeanDefinitionRegistry;
-import org.springframework.beans.factory.support.DefaultListableBeanFactory;
-import org.springframework.context.annotation.Bean;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
@@ -22,6 +14,9 @@ import org.springframework.web.context.WebApplicationContext;
 
 import javax.inject.Inject;
 import java.math.BigInteger;
+import java.util.List;
+
+import static org.junit.Assert.*;
 
 /**
  * Created by Damien on 1/19/2016.
@@ -32,50 +27,118 @@ import java.math.BigInteger;
 public class CharacterMongoRepositoryTest {
     @Inject
     private CharacterMongoRepository repository;
-    static WebApplicationContext applicationContext;
     @Inject
+    static WebApplicationContext applicationContext;
 
-    @AfterClass
-    public static void tearDown(){
+    @After
+    public void tearDown() {
         applicationContext.getAutowireCapableBeanFactory().destroyBean(applicationContext.getAutowireCapableBeanFactory().getBean("pluginManagerImpl"));
     }
 
     @Test
     public void testGetCharacter() {
-        Character character = new Character() {
-            private BigInteger id;
-            private PluginDescription pluginDescription;
-
-            @Override
-            public BigInteger getId() {
-                return id;
-            }
-
-            @Override
-            public void setId(BigInteger id) {
-                this.id = id;
-            }
-
-            @Override
-            public PluginDescription getPluginDescription() {
-                return pluginDescription;
-            }
-
-            @Override
-            public void setPluginDescription(PluginDescription pluginDescription) {
-                this.pluginDescription = pluginDescription;
-            }
-        };
+        CharacterOne character = new CharacterOne();
+        CharacterTwo charactertwo = new CharacterTwo();
+        character.setId(BigInteger.ONE);
+        charactertwo.setId(BigInteger.TEN);
         repository.save(character);
-    }
-
-    @Bean
-    public PluginManager PluginManager(){
-        return Mockito.mock(PluginManager.class);
+        assertEquals(character, repository.findOne(character.getId()));
+        repository.save(charactertwo);
+        List<Character> characterList = repository.findAll();
+        assertEquals(charactertwo, repository.findOne(charactertwo.getId()));
     }
 
     @Inject
-    public void setApplicationContext(WebApplicationContext context){
+    public void setApplicationContext(WebApplicationContext context) {
         applicationContext = context;
     }
+
+    public static class CharacterOne implements io.github.thisisnozaku.charactercreator.plugins.Character {
+        private BigInteger id;
+        private PluginDescription pluginDescription;
+
+        @Override
+        public BigInteger getId() {
+            return id;
+        }
+
+        @Override
+        public void setId(BigInteger id) {
+            this.id = id;
+        }
+
+        @Override
+        public PluginDescription getPluginDescription() {
+            return pluginDescription;
+        }
+
+        @Override
+        public void setPluginDescription(PluginDescription pluginDescription) {
+            this.pluginDescription = pluginDescription;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+
+            CharacterOne character = (CharacterOne) o;
+
+            if (id != null ? !id.equals(character.id) : character.id != null) return false;
+            return pluginDescription != null ? pluginDescription.equals(character.pluginDescription) : character.pluginDescription == null;
+
+        }
+
+        @Override
+        public int hashCode() {
+            int result = id != null ? id.hashCode() : 0;
+            result = 31 * result + (pluginDescription != null ? pluginDescription.hashCode() : 0);
+            return result;
+        }
+    }
+
+    public static class CharacterTwo implements io.github.thisisnozaku.charactercreator.plugins.Character {
+        private BigInteger id;
+        private PluginDescription pluginDescription;
+
+        @Override
+        public BigInteger getId() {
+            return id;
+        }
+
+        @Override
+        public void setId(BigInteger id) {
+            this.id = id;
+        }
+
+        @Override
+        public PluginDescription getPluginDescription() {
+            return pluginDescription;
+        }
+
+        @Override
+        public void setPluginDescription(PluginDescription pluginDescription) {
+            this.pluginDescription = pluginDescription;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+
+            CharacterTwo character = (CharacterTwo) o;
+
+            if (id != null ? !id.equals(character.id) : character.id != null) return false;
+            return pluginDescription != null ? pluginDescription.equals(character.pluginDescription) : character.pluginDescription == null;
+
+        }
+
+        @Override
+        public int hashCode() {
+            int result = id != null ? id.hashCode() : 0;
+            result = 31 * result + (pluginDescription != null ? pluginDescription.hashCode() : 0);
+            return result;
+        }
+    }
+
 }
