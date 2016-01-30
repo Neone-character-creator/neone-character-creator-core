740ca1bb3b587cef05d5cc8a39d2a43cae1444ae
Damien <damienmarble@hotmail.com>
2016-01-29 17:06:38 -0800

    Added Interceptor for incoming requests that checks if the plugin the request targets is available.

--- src/main/java/io/github/thisisnozaku/charactercreator/config/WebConfig.java
+++ src/main/java/io/github/thisisnozaku/charactercreator/config/WebConfig.java
@@ -2,9 +2,12 @@ package io.github.thisisnozaku.charactercreator.config;
 
 import io.github.thisisnozaku.charactercreator.plugins.CharacterResolver;
 import io.github.thisisnozaku.charactercreator.plugins.PluginManager;
+import io.github.thisisnozaku.charactercreator.plugins.PluginPresenceInterceptor;
 import org.springframework.boot.autoconfigure.web.WebMvcAutoConfiguration;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerMapping;
 
 import javax.inject.Inject;
 import java.util.List;
@@ -14,12 +17,18 @@ import java.util.List;
  */
 @Configuration
 public class WebConfig extends WebMvcAutoConfiguration.WebMvcAutoConfigurationAdapter {
-
     @Inject
-    PluginManager pluginManager;
+    private PluginManager pluginManager;
+    @Inject
+    private PluginPresenceInterceptor pluginPresenceInterceptor;
 
     @Override
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> argumentResolvers) {
         argumentResolvers.add(new CharacterResolver(pluginManager));
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry interceptorRegistry){
+        interceptorRegistry.addInterceptor(pluginPresenceInterceptor);
     }
 }
---
+++ src/main/java/io/github/thisisnozaku/charactercreator/plugins/PluginPresenceInterceptor.java
@@ -0,0 +1,42 @@
+package io.github.thisisnozaku.charactercreator.plugins;
+
+import io.github.thisisnozaku.charactercreator.exceptions.MissingPluginException;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+import javax.inject.Inject;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.net.URLDecoder;
+import java.util.Map;
+import java.util.Optional;
+
+/**
+ * Created by Damien on 1/23/2016.
+ */
+@Component
+public class PluginPresenceInterceptor extends HandlerInterceptorAdapter {
+    private PluginManager pluginManager;
+
+    @Inject
+    public PluginPresenceInterceptor(PluginManager pluginManager){
+        this.pluginManager = pluginManager;
+    }
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        Map<String, String> templateParameters = (Map<String, String>) request.getAttribute("org.springframework.web.servlet.HandlerMapping.uriTemplateVariables");
+        String author = URLDecoder.decode(templateParameters.get("author"), "UTF-8");
+        String game = URLDecoder.decode(templateParameters.get("game"), "UTF-8");
+        String version = URLDecoder.decode(templateParameters.get("version"), "UTF-8");
+        Optional<GamePlugin> plugin = pluginManager.getPlugin(author,game,version);
+        if (plugin.isPresent()){
+            return true;
+        }
+        throw new MissingPluginException();
+    }
+}
