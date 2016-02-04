82299cd53f6bb6eb0c2253981ae4c5b20bd0c888
Damien <damienmarble@hotmail.com>
2016-02-04 12:56:34 -0800

    Added validation to Account.

--- src/main/java/io/github/thisisnozaku/charactercreator/model/Account.java
+++ src/main/java/io/github/thisisnozaku/charactercreator/model/Account.java
@@ -1,9 +1,12 @@
 package io.github.thisisnozaku.charactercreator.model;
 
+import org.hibernate.validator.constraints.NotEmpty;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
 
 /**
  * Created by Damien on 11/13/2015.
@@ -13,24 +16,57 @@ public class Account {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
-    private final String username;
+    @NotNull
+    @NotEmpty
+    private String username;
+    @NotNull
+    @NotEmpty
+    private String password;
+    private String passwordConfirm;
+    @NotNull
+    @NotEmpty
+    private String email;
 
-    public Account(String username) {
-        this.username = username;
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        if (this.id == null) {
+            throw new IllegalStateException("Attempted to reassign the id of a user.");
+        }
+        this.id = id;
     }
 
     public String getUsername() {
         return username;
     }
 
-    public long getId() {
-        return id;
+    public void setUsername(String username) {
+        this.username = username;
     }
 
-    public void setId(long id) {
-        if(this.id == null) {
-            throw new IllegalStateException("Attempted to reassign the id of a user.");
-        }
-        this.id = id;
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getPasswordConfirm() {
+        return passwordConfirm;
+    }
+
+    public void setPasswordConfirm(String passwordConfirm) {
+        this.passwordConfirm = passwordConfirm;
     }
 }
