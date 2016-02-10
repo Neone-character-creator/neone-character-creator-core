88f19468c582f477f9a7831d87dafa9341777252
Damien <damienmarble@hotmail.com>
2016-02-10 09:51:12 -0800

    Modified CharacterMongoConverter to override both version of read()

--- src/main/java/io/github/thisisnozaku/charactercreator/data/CharacterMongoConverter.java
+++ src/main/java/io/github/thisisnozaku/charactercreator/data/CharacterMongoConverter.java
@@ -11,6 +11,7 @@ import org.springframework.data.mongodb.core.convert.MappingMongoConverter;
 import org.springframework.data.mongodb.core.mapping.MongoPersistentEntity;
 import org.springframework.data.mongodb.core.mapping.MongoPersistentProperty;
 import org.springframework.data.util.ClassTypeInformation;
+import org.springframework.data.util.TypeInformation;
 import org.springframework.stereotype.Component;
 
 import javax.inject.Inject;
@@ -28,14 +29,25 @@ public class CharacterMongoConverter extends MappingMongoConverter {
 
     @Override
     public <T> T read(Class<T> clazz, DBObject dbo) {
-        if (Character.class.isAssignableFrom(clazz)){
-            BasicDBObject object = (BasicDBObject) dbo.get("pluginDescription");
-            PluginDescription pluginDescription = new PluginDescription(object.getString("author"), object.getString("system"), object.getString("version"));
-            return (T) super.read(ClassTypeInformation.from(pluginManager.getPlugin(pluginDescription).get().getCharacterType()), dbo);
+        if (Character.class.isAssignableFrom(clazz)) {
+            return (T) super.read(getTypeForObject(dbo), dbo);
         } else {
             return super.read(clazz, dbo);
         }
     }
 
+    @Override
+    protected <S> S read(TypeInformation<S> type, DBObject dbo) {
+        if (type.getType().isAssignableFrom(Character.class)) {
+            return (S) super.read(ClassTypeInformation.from(getTypeForObject(dbo)), dbo);
+        } else {
+            return super.read(type, dbo);
+        }
+    }
 
+    private Class<?> getTypeForObject(DBObject dbo){
+        BasicDBObject object = (BasicDBObject) dbo.get("pluginDescription");
+        PluginDescription pluginDescription = new PluginDescription(object.getString("author"), object.getString("system"), object.getString("version"));
+        return pluginManager.getPlugin(pluginDescription).get().getCharacterType();
+    }
 }
