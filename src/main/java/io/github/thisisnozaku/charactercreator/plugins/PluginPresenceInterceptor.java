243ad011fb34cc5def1266880c770ec5415e0d6f
Damien <damienmarble@hotmail.com>
2016-02-13 20:19:12 -0800

    Fixed PluginPresenceInterceptor  so that it works regardless of the Controller used.

--- src/main/java/io/github/thisisnozaku/charactercreator/plugins/PluginPresenceInterceptor.java
+++ src/main/java/io/github/thisisnozaku/charactercreator/plugins/PluginPresenceInterceptor.java
@@ -1,12 +1,9 @@
 package io.github.thisisnozaku.charactercreator.plugins;
 
-import io.github.thisisnozaku.charactercreator.controllers.GameController;
+import io.github.thisisnozaku.charactercreator.controllers.GamePagesController;
 import io.github.thisisnozaku.charactercreator.exceptions.MissingPluginException;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Component;
-import org.springframework.web.servlet.HandlerInterceptor;
-import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.method.HandlerMethod;
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 
 import javax.inject.Inject;
@@ -30,16 +27,16 @@ public class PluginPresenceInterceptor extends HandlerInterceptorAdapter {
 
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
-        if (handler instanceof GameController) {
-            Map<String, String> templateParameters = (Map<String, String>) request.getAttribute("org.springframework.web.servlet.HandlerMapping.uriTemplateVariables");
+        HandlerMethod handlerMethod = (HandlerMethod) handler;
+        Map<String, String> templateParameters = (Map<String, String>) request.getAttribute("org.springframework.web.servlet.HandlerMapping.uriTemplateVariables");
+        if (!templateParameters.isEmpty()) {
             String author = URLDecoder.decode(templateParameters.get("author"), "UTF-8");
             String game = URLDecoder.decode(templateParameters.get("game"), "UTF-8");
             String version = URLDecoder.decode(templateParameters.get("version"), "UTF-8");
             Optional<GamePlugin> plugin = pluginManager.getPlugin(author, game, version);
-            if (plugin.isPresent()) {
-                return true;
+            if (!plugin.isPresent()) {
+                throw new MissingPluginException();
             }
-            throw new MissingPluginException();
         }
         return true;
     }
