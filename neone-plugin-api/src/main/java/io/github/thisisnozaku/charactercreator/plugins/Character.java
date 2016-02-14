5290e3d9111dc2f6e766bd5e6a038ef77f9153c2
Damien <damienmarble@hotmail.com>
2016-02-13 20:25:03 -0800

    Changed Character from an interface to an abstract class.

--- neone-plugin-api/src/main/java/io/github/thisisnozaku/charactercreator/plugins/Character.java
+++ neone-plugin-api/src/main/java/io/github/thisisnozaku/charactercreator/plugins/Character.java
@@ -6,5 +6,14 @@ import java.math.BigInteger;
 /**
  * Created by Damien on 11/22/2015.
  */
-public interface Character {
+public abstract class Character {
+    private PluginDescription pluginDescription;
+
+    public PluginDescription getPluginDescription() {
+        return pluginDescription;
+    }
+
+    public void setPluginDescription(PluginDescription pluginDescription) {
+        this.pluginDescription = pluginDescription;
+    }
 }
