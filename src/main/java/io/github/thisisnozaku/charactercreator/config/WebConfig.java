d2be43994dedd27bae4dfb16e8c506bfa696c87d
Damien <damienmarble@hotmail.com>
2016-02-13 20:16:03 -0800

    Cleaned up imports.

--- src/main/java/io/github/thisisnozaku/charactercreator/config/WebConfig.java
+++ src/main/java/io/github/thisisnozaku/charactercreator/config/WebConfig.java
@@ -7,7 +7,6 @@ import org.springframework.boot.autoconfigure.web.WebMvcAutoConfiguration;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
-import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerMapping;
 
 import javax.inject.Inject;
 import java.util.List;
