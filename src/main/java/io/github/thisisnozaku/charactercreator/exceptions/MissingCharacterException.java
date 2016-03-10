6d49a79500e0edba0bc3d64176cecb7ba5605f29
Damien <damienmarble@hotmail.com>
2016-03-09 20:54:48 -0800

    Added 404 ResponseStatus to MissingCharacterException.

--- src/main/java/io/github/thisisnozaku/charactercreator/exceptions/MissingCharacterException.java
+++ src/main/java/io/github/thisisnozaku/charactercreator/exceptions/MissingCharacterException.java
@@ -1,9 +1,13 @@
 package io.github.thisisnozaku.charactercreator.exceptions;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
 /**
  * Exception generated when attempting to view a Character that isn't available.
  * Created by Damien on 12/7/2015.
  */
+@ResponseStatus(HttpStatus.NOT_FOUND)
 public class MissingCharacterException extends RuntimeException {
 
 }
