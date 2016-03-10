4754b092205d1205d0a30482db633536cf0e72c4
Damien <damienmarble@hotmail.com>
2016-03-09 20:54:26 -0800

    Added custom method for getting by username.

--- src/main/java/io/github/thisisnozaku/charactercreator/data/UserRepository.java
+++ src/main/java/io/github/thisisnozaku/charactercreator/data/UserRepository.java
@@ -1,11 +1,11 @@
 package io.github.thisisnozaku.charactercreator.data;
 
-import io.github.thisisnozaku.charactercreator.model.User;
+import io.github.thisisnozaku.charactercreator.authentication.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 /**
  * Created by Damien on 11/15/2015.
  */
 public interface UserRepository extends JpaRepository<User, Long> {
-
+    public User findByUsername(String username);
 }
