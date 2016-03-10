d6209fe2b93e3ef47b6ce2aa63dda1e116fd529a
Damien <damienmarble@hotmail.com>
2016-03-09 20:53:04 -0800

    Added ResponseStatus to CharacterPluginMismatchException for 406 code.

--- src/main/java/io/github/thisisnozaku/charactercreator/exceptions/CharacterPluginMismatchException.java
+++ src/main/java/io/github/thisisnozaku/charactercreator/exceptions/CharacterPluginMismatchException.java
@@ -1,11 +1,14 @@
 package io.github.thisisnozaku.charactercreator.exceptions;
 
 import io.github.thisisnozaku.charactercreator.plugins.PluginDescription;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
 /**
  * Exception generated when attempting to perform an action, when the plugin that is required is
  * Created by Damien on 12/7/2015.
  */
+@ResponseStatus(HttpStatus.NOT_ACCEPTABLE)
 public class CharacterPluginMismatchException extends RuntimeException {
     private final PluginDescription requiredPlugin;
     private final PluginDescription actualPlugin;
