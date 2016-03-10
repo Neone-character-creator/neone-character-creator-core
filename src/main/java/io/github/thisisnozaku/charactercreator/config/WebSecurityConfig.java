7372f7d199f8667fa180472cc5c674884eadc585
Damien <damienmarble@hotmail.com>
2016-03-09 20:58:17 -0800

    Added Security configuration class.

---
+++ src/main/java/io/github/thisisnozaku/charactercreator/config/WebSecurityConfig.java
@@ -0,0 +1,56 @@
+package io.github.thisisnozaku.charactercreator.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.csrf.CsrfTokenRepository;
+import org.springframework.security.web.csrf.HttpSessionCsrfTokenRepository;
+
+import javax.inject.Inject;
+import javax.sql.DataSource;
+
+/**
+ * Created by Damien on 1/30/2016.
+ */
+@Configuration
+@EnableWebSecurity
+public class
+WebSecurityConfig extends WebSecurityConfigurerAdapter {
+    @Inject
+    UserDetailsService userDetailsService;
+
+    @Override
+    protected void configure(HttpSecurity security) throws Exception {
+        security.csrf().csrfTokenRepository(csrfTokenRepository());
+
+        security.authorizeRequests().antMatchers("/", "/login", "/createuser").permitAll()
+                .antMatchers("/**").authenticated()
+                .and()
+                .formLogin().loginPage("/login").failureUrl("/login?error").and()
+                .logout().permitAll().and();
+    }
+
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder()).and()
+        .inMemoryAuthentication().withUser("Damien").password("Paranoia").roles("User");
+    }
+
+    private CsrfTokenRepository csrfTokenRepository() {
+        HttpSessionCsrfTokenRepository repository = new HttpSessionCsrfTokenRepository();
+        repository.setSessionAttributeName("_csrf");
+        return repository;
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
