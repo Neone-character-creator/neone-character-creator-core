4c26f626e01273ae15669cd05bd54012c300fa47
Damien <damienmarble@hotmail.com>
2016-03-23 11:32:08 -0700

    Renamed file.

--- src/main/java/io/github/thisisnozaku/charactercreator/controllers/GamepAGESController.java
+++ src/main/java/io/github/thisisnozaku/charactercreator/controllers/GamepAGESController.java
@@ -3,28 +3,24 @@ package io.github.thisisnozaku.charactercreator.controllers;
 import io.github.thisisnozaku.charactercreator.data.CharacterDataWrapper;
 import io.github.thisisnozaku.charactercreator.data.CharacterMongoRepository;
 import io.github.thisisnozaku.charactercreator.data.UserRepository;
-import io.github.thisisnozaku.charactercreator.exceptions.CharacterPluginMismatchException;
-import io.github.thisisnozaku.charactercreator.exceptions.MissingCharacterException;
 import io.github.thisisnozaku.charactercreator.exceptions.MissingPluginException;
+import io.github.thisisnozaku.charactercreator.authentication.User;
 import io.github.thisisnozaku.charactercreator.plugins.Character;
 import io.github.thisisnozaku.charactercreator.plugins.GamePlugin;
 import io.github.thisisnozaku.charactercreator.plugins.PluginDescription;
 import io.github.thisisnozaku.charactercreator.plugins.PluginManager;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.ModelAndView;
 
 import javax.inject.Inject;
 import java.io.UnsupportedEncodingException;
-import java.math.BigDecimal;
 import java.math.BigInteger;
 import java.net.URLDecoder;
-import java.util.List;
 import java.util.NoSuchElementException;
 import java.util.Optional;
-import java.util.stream.Collectors;
 
 /**
  * Controller for handling access to game plugin pages.
@@ -32,7 +28,7 @@ import java.util.stream.Collectors;
  * Created by Damien on 11/15/2015.
  */
 @Controller
-@RequestMapping("games/{author}/{game}/{version:.+}/pages")
+@RequestMapping("games/{author}/{game}/{version:.+}/")
 public class GamePagesController {
     private final UserRepository accounts;
     private final CharacterMongoRepository characters;
@@ -45,7 +41,12 @@ public class GamePagesController {
         this.plugins = plugins;
     }
 
-    @RequestMapping(value = "/info", method = RequestMethod.GET, produces = "text/html")
+    @RequestMapping(value={"/", ""})
+    public String redirect() {
+        return "redirect:pages/info";
+    }
+
+    @RequestMapping(value = "/pages/info", method = RequestMethod.GET, produces = "text/html")
     public String description(@PathVariable("author") String author, @PathVariable("game") String game, @PathVariable("version") String version, Model model) throws UnsupportedEncodingException {
         try {
             author = URLDecoder.decode(author, "UTF-8");
@@ -66,8 +67,8 @@ public class GamePagesController {
         return "plugin-character-page";
     }
 
-    @RequestMapping(value = "/character", method = RequestMethod.GET, produces = "text/html")
-    public String getCharacter(Character character, @PathVariable("author") String author, @PathVariable("game") String game, @PathVariable("version") String version, Model model, @RequestParam(required = false) BigInteger id) {
+    @RequestMapping(value = "/pages/character", method = RequestMethod.GET, produces = "text/html")
+    public String getCharacter(Character character, @PathVariable("author") String author, @PathVariable("game") String game, @PathVariable("version") String version, Model model, @RequestParam(required = false) BigInteger id, @AuthenticationPrincipal User user) {
         try {
             author = URLDecoder.decode(author, "UTF-8");
             game = URLDecoder.decode(game, "UTF-8");
@@ -75,18 +76,18 @@ public class GamePagesController {
         } catch (UnsupportedEncodingException ex) {
             throw new IllegalStateException(ex);
         }
-        if (id != null) {
-            PluginDescription description = new PluginDescription(author, game, version);
-            Optional<GamePlugin> plugin = plugins.getPlugin(author, game, version);
-            if (plugin.isPresent()) {
-                CharacterDataWrapper wrapper = new CharacterDataWrapper(description, null, character);
-                character = characters.findOne(id).getCharacter();
-                model.addAttribute("characterid", wrapper.getId());
-            } else {
-                throw new MissingPluginException();
-            }
+        PluginDescription description = new PluginDescription(author, game, version);
+        Optional<GamePlugin> plugin = plugins.getPlugin(author, game, version);
+        if (!plugin.isPresent()) {
+            throw new MissingPluginException();
         }
-        model.addAttribute("character", character);
+        CharacterDataWrapper wrapper;
+        if (id != null) {
+            wrapper = characters.findOne(id);
+        } else {
+            wrapper = new CharacterDataWrapper(description, user, character);
+        }
+        model.addAttribute("wrapper", wrapper);
         model.addAttribute("author", author);
         model.addAttribute("game", game);
         model.addAttribute("version", version);
@@ -98,7 +99,10 @@ public class GamePagesController {
 
     @ExceptionHandler(MissingPluginException.class)
     @ResponseStatus(HttpStatus.NOT_FOUND)
-    public String missingPlugin() {
+    public String missingPlugin(Model model, @PathVariable("author") String author, @PathVariable("game") String game, @PathVariable("version") String version) {
+        model.addAttribute("author", author);
+        model.addAttribute("game", game);
+        model.addAttribute("version", version);
         return "missing-plugin";
     }
 }
