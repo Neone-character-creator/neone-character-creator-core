85c4b3b61ca06ab092156f3ec4b4758c32d81f9a
Damien <damienmarble@gmail.com>
2016-04-21 00:12:54 -0700

    Added password encoding to registration method in SecurityController.

--- src/main/java/io/github/thisisnozaku/charactercreator/controllers/SecurityController.java
+++ src/main/java/io/github/thisisnozaku/charactercreator/controllers/SecurityController.java
@@ -1,7 +1,9 @@
 package io.github.thisisnozaku.charactercreator.controllers;
 
+import io.github.thisisnozaku.charactercreator.authentication.AppAuthority;
 import io.github.thisisnozaku.charactercreator.data.UserRepository;
 import io.github.thisisnozaku.charactercreator.authentication.User;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestHeader;
@@ -11,6 +13,8 @@ import org.springframework.web.context.request.WebRequest;
 
 import javax.inject.Inject;
 import javax.servlet.http.HttpServletRequest;
+import java.util.Collection;
+import java.util.HashSet;
 
 /**
  * Created by Damien on 1/31/2016.
@@ -18,10 +22,16 @@ import javax.servlet.http.HttpServletRequest;
 @Controller
 public class SecurityController {
     @Inject
-    UserRepository users;
+    private UserRepository users;
+    @Inject
+    private PasswordEncoder passwordEncoder;
 
     @RequestMapping(value = "/createuser", method = RequestMethod.POST)
     public String register(User user, @RequestHeader(value = "referer", required = false)String referralUrl) {
+        Collection<AppAuthority> authorities = new HashSet<>();
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        authorities.add(new AppAuthority(null, "USER"));
+        user.setAuthorities(authorities);
         users.save(user);
         return "index";
     }
