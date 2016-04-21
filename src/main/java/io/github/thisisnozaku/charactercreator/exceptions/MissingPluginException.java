d6d280c5a8f645f2befc068e4e00e630f4568179
Damien <damienmarble@gmail.com>
2016-04-21 00:17:18 -0700

    Changed MissingPluginException so that it contains the PluginDescription of the missing plugin.

--- src/main/java/io/github/thisisnozaku/charactercreator/exceptions/MissingPluginException.java
+++ src/main/java/io/github/thisisnozaku/charactercreator/exceptions/MissingPluginException.java
@@ -1,9 +1,18 @@
 package io.github.thisisnozaku.charactercreator.exceptions;
 
+import io.github.thisisnozaku.charactercreator.plugins.PluginDescription;
+
 /**
  * Exception generated when trying to access a plugin that is not available.
  * Created by Damien on 12/7/2015.
  */
 public class MissingPluginException extends RuntimeException {
+    private final PluginDescription missingPlugin;
+    public MissingPluginException(PluginDescription missingPlugin){
+        this.missingPlugin = missingPlugin;
+    }
 
+    public PluginDescription getMissingPlugin() {
+        return missingPlugin;
+    }
 }
