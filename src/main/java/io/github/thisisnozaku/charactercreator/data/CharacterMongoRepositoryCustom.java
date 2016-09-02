943a88218e3352a80bc24f46fbe8cb1f5eb3c63a
Damien <damienmarble@gmail.com>
2016-09-02 02:51:13 -0700

    Cleaned up imports. Custom character mongo interface changed to use Object for the user.

--- src/main/java/io/github/thisisnozaku/charactercreator/data/CharacterMongoRepositoryCustom.java
+++ src/main/java/io/github/thisisnozaku/charactercreator/data/CharacterMongoRepositoryCustom.java
@@ -1,16 +1,14 @@
 package io.github.thisisnozaku.charactercreator.data;
 
-import io.github.thisisnozaku.charactercreator.authentication.User;
 import io.github.thisisnozaku.charactercreator.plugins.PluginDescription;
 import org.springframework.data.mongodb.repository.Query;
 
-import java.security.Principal;
 import java.util.List;
 
 /**
  * Created by Damien on 4/17/2016.
  */
 public interface CharacterMongoRepositoryCustom extends CharacterMongoRepository {
-    @Query()
-    List<CharacterDataWrapper> findByUserAndPlugin(User user, PluginDescription plugin);
+    @Query
+    List<CharacterDataWrapper> findByUserAndPlugin(Object user, PluginDescription plugin);
 }
