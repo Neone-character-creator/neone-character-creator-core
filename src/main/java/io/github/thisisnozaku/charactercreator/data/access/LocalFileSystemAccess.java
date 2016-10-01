83f346f139db6617a9267c15cc160bc66cbe3a0d
Damien <damienmarble@gmail.com>
2016-10-01 13:55:38 -0700

    Added annotation to LocalFileSystemAccess so that it is registered only in dev profile.

--- src/main/java/io/github/thisisnozaku/charactercreator/data/access/LocalFileSystemAccess.java
+++ src/main/java/io/github/thisisnozaku/charactercreator/data/access/LocalFileSystemAccess.java
@@ -1,7 +1,7 @@
 package io.github.thisisnozaku.charactercreator.data.access;
 
-import org.apache.felix.scr.annotations.Service;
 import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
 
 import java.io.IOException;
 import java.io.InputStream;
