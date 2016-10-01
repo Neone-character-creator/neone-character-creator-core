00f10dd3f83dc90928a45d2be8278fa32ecb54c6
Damien <damienmarble@gmail.com>
2016-10-01 13:46:28 -0700

    Renamed MailSender to AppMailSender.

--- src/main/java/io/github/thisisnozaku/charactercreator/mail/AppMailSender.java
+++ src/main/java/io/github/thisisnozaku/charactercreator/mail/AppMailSender.java
@@ -1,29 +1,19 @@
 package io.github.thisisnozaku.charactercreator.mail;
 
-import com.amazonaws.ClientConfiguration;
-import com.amazonaws.auth.AWSCredentials;
-import com.amazonaws.auth.DefaultAWSCredentialsProviderChain;
-import com.amazonaws.regions.Region;
-import com.amazonaws.regions.Regions;
-import com.amazonaws.services.simpleemail.AmazonSimpleEmailServiceAsyncClient;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Profile;
+import org.springframework.context.annotation.Bean;
 import org.springframework.mail.MailException;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 
-import javax.annotation.PostConstruct;
 import javax.inject.Inject;
 import javax.mail.internet.MimeMessage;
-import java.util.Properties;
 
 /**
  * Created by Damien on 9/15/2016.
  */
 @Service
-public class MailSender implements EmailSender {
-    private AmazonSimpleEmailServiceAsyncClient client;
-    private Properties sessionProperties;
+public class AppMailSender implements EmailSender {
     @Inject
     private JavaMailSender mailSender;
 
@@ -42,17 +32,8 @@ public class MailSender implements EmailSender {
     @Value("${spring.mail.properties.mail.smtp.starttls.required}")
     private String requiredTls;
 
-    @PostConstruct
-    public void init() {
-        AWSCredentials credentials = new DefaultAWSCredentialsProviderChain().getCredentials();
-        ClientConfiguration config = new ClientConfiguration();
-        this.client = new AmazonSimpleEmailServiceAsyncClient();
-        client.setRegion(Region.getRegion(Regions.US_WEST_2));
-
-        sessionProperties = new Properties();
-        sessionProperties.setProperty("mail.smtp.auth", auth);
-        sessionProperties.setProperty("mail.smtp.starttls.enable", enableTls);
-        sessionProperties.setProperty("mail.smtp.starttls.required", requiredTls);
+    public AppMailSender() {
+        super();
     }
 
     public void sendMail(MimeMessage incomingMessage) {
