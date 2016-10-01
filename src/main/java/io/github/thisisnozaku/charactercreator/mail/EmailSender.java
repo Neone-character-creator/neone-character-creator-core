acf832bcff7909a249efc6080238ffda5a1aeda6
Damien <damienmarble@gmail.com>
2016-10-01 13:49:33 -0700

    Added @Service annotation.

--- src/main/java/io/github/thisisnozaku/charactercreator/mail/EmailSender.java
+++ src/main/java/io/github/thisisnozaku/charactercreator/mail/EmailSender.java
@@ -1,11 +1,13 @@
 package io.github.thisisnozaku.charactercreator.mail;
 
-import javax.mail.Message;
+import org.springframework.stereotype.Service;
+
 import javax.mail.internet.MimeMessage;
 
 /**
  * Created by Damien on 9/15/2016.
  */
+@Service
 public interface EmailSender {
     void sendMail(MimeMessage message);
 }
