0466821e746072f61380370c4323570cfde8e584
Damien <damienmarble@gmail.com>
2016-10-04 12:50:43 -0700

    Enabled proxies on NeoneCoreApplication.

--- src/main/java/io/github/thisisnozaku/charactercreator/NeoneCoreApplication.java
+++ src/main/java/io/github/thisisnozaku/charactercreator/NeoneCoreApplication.java
@@ -1,10 +1,12 @@
 package io.github.thisisnozaku.charactercreator;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 import static org.springframework.boot.SpringApplication.run;
 
 @SpringBootApplication
+@EnableAspectJAutoProxy
 public class NeoneCoreApplication {
 
     public static void main(String[] args) {
