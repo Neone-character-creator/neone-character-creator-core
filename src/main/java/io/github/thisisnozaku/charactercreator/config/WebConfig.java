61a2896068354aebfff7ae20f121812456f1d819
Damien <damienmarble@gmail.com>
2016-10-05 04:03:54 -0700

    Disabled all caching for plugin resources.
    This fixes a bug where if a request gets a plugin resource, said plugin is removed and it attempts to re-request the resource with caching enabled, it will ask Spring to validate the cache, Spring will try to access the URL of the bundle which no longer exists and fail with an exception.
    
    Possibly an opportunity for a custom caching validator implementation.

--- src/main/java/io/github/thisisnozaku/charactercreator/config/WebConfig.java
+++ src/main/java/io/github/thisisnozaku/charactercreator/config/WebConfig.java
@@ -9,6 +9,7 @@ import org.springframework.boot.autoconfigure.web.WebMvcAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.http.CacheControl;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
@@ -46,7 +47,7 @@ public class WebConfig extends WebMvcAutoConfiguration.WebMvcAutoConfigurationAd
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("**/pluginresource/**")
+        registry.addResourceHandler("**/pluginresource/**").setCacheControl(CacheControl.noStore())
                 .resourceChain(true).addResolver(pluginResourceResolver);
 
         super.addResourceHandlers(registry);
