49609748e3f298d96164954f8e5f6bc7ed6fecbd
Damien Marble <damienmarble@gmail.com>
2017-02-01 14:36:27 -0700

    Cleaned up imports in AWSConfig.

--- src/main/java/io/github/thisisnozaku/charactercreator/config/AWSConfig.java
+++ src/main/java/io/github/thisisnozaku/charactercreator/config/AWSConfig.java
@@ -3,14 +3,12 @@ package io.github.thisisnozaku.charactercreator.config;
 import com.amazonaws.regions.Region;
 import com.amazonaws.regions.Regions;
 import com.amazonaws.services.s3.AmazonS3;
-import com.amazonaws.services.s3.AmazonS3Builder;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import com.amazonaws.services.sqs.AmazonSQS;
 import com.amazonaws.services.sqs.AmazonSQSClientBuilder;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
 
 /**
  * Created by Damie on 1/30/2017.
