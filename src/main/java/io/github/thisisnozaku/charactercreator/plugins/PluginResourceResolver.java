fd845bd45b03289e07070df7bb825822f8512cce
Damien Marble <damienmarble@gmail.com>
2017-02-08 13:03:17 -0700

    Cleaned up imports.
    Suppressed warning on injected pluginManager field.
    
    Changed returning resource directly out of option in resolveResource.

--- src/main/java/io/github/thisisnozaku/charactercreator/plugins/PluginResourceResolver.java
+++ src/main/java/io/github/thisisnozaku/charactercreator/plugins/PluginResourceResolver.java
@@ -2,35 +2,27 @@ package io.github.thisisnozaku.charactercreator.plugins;
 
 import org.springframework.core.io.Resource;
 import org.springframework.core.io.UrlResource;
-import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
-import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
-import org.springframework.web.servlet.resource.AbstractResourceResolver;
-import org.springframework.web.servlet.resource.PathResourceResolver;
 import org.springframework.web.servlet.resource.ResourceResolver;
 import org.springframework.web.servlet.resource.ResourceResolverChain;
-import org.thymeleaf.resourceresolver.UrlResourceResolver;
 
 import javax.inject.Inject;
 import javax.servlet.http.HttpServletRequest;
-import java.io.IOException;
 import java.io.UnsupportedEncodingException;
 import java.net.MalformedURLException;
 import java.net.URI;
-import java.net.URL;
 import java.net.URLDecoder;
 import java.util.Arrays;
 import java.util.List;
-import java.util.Map;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 /**
  * Resolves resources that are found in plugins.
  */
 @Service
-public class PluginResourceResolver implements ResourceResolver {
+public class PluginResourceResolver implements ResourceResolver{
+    @SuppressWarnings({"CanBeFinal", "unused"})
     @Inject
     private PluginManager pluginManager;
 
@@ -46,17 +38,18 @@ public class PluginResourceResolver implements ResourceResolver {
             PluginDescription incomingPluginDescription = new PluginDescription(URLDecoder.decode(author, "UTF-8"),
                     URLDecoder.decode(game, "UTF-8"),
                     URLDecoder.decode(version, "UTF-8"));
-            //If there is no resource is named, we get the character sheet
+            //If no resource is named, we get the character sheet
             if(resourcePath.equals("")){
                 resourcePath = "character";
             }
-            URL resourceUri = null;
+            Optional<URI> resource = pluginManager.getPluginResource(incomingPluginDescription, resourcePath);
+
             try {
-                resourceUri = pluginManager.getPluginResource(incomingPluginDescription, resourcePath).toURL();
+                return new UrlResource(resource.orElse(null));
             }catch (MalformedURLException ex){
                 throw new RuntimeException(ex);
             }
-            return resourceUri != null ? new UrlResource(resourceUri) : null;
+
         } catch (UnsupportedEncodingException e) {
             throw new IllegalStateException(e);
         }
