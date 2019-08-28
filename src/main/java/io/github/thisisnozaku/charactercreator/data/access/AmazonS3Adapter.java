ae87e0da8efc27fc6ef261eb34b20ba4f09c3506
Damie <damienmarble@gmail.com>
2019-08-27 22:47:49 -0400

    Even more path problems.

--- src/main/java/io/github/thisisnozaku/charactercreator/data/access/AmazonS3Adapter.java
+++ src/main/java/io/github/thisisnozaku/charactercreator/data/access/AmazonS3Adapter.java
@@ -90,7 +90,9 @@ public class AmazonS3Adapter implements FileAccessor {
         public S3BackedFileInformation(String resourcePath) {
             logger.info("Creating S3-backed FileInformation for path {}", resourcePath);
             resourcePath = (resourcePath.startsWith("/") ? resourcePath.substring(1) : resourcePath);
-            resourcePath = resourcePath.substring(resourcePath.indexOf("s3.amazonaws.com/") + "s3.amazonaws.com/".length());
+            resourcePath = resourcePath.contains("s3.amazonaws.com/") ?
+                    resourcePath.substring(resourcePath.indexOf("s3.amazonaws.com/") + "s3.amazonaws.com/".length()) :
+                    resourcePath;
             logger.info("Adjusted resourcePath {}", resourcePath);
             this.objectKey = resourcePath;
         }
