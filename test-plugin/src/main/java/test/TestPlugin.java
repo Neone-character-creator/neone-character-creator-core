1ed106a4904d45858422d8756cb116699973d97e
Damie <damienmarble@gmail.com>
2019-11-08 20:25:51 -0500

    Fix for service not automatically starting.

--- test-plugin/src/main/java/test/TestPlugin.java
+++ test-plugin/src/main/java/test/TestPlugin.java
@@ -5,7 +5,7 @@ import org.apache.felix.scr.annotations.Component;
 import org.apache.felix.scr.annotations.Service;
 
 @Service(value = GamePlugin.class)
-@Component
+@Component(immediate = true)
 public class TestPlugin extends GamePlugin {
 
 }
