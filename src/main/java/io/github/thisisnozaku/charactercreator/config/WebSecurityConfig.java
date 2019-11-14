ec049170b0e156ad9d3c41ab5fd7cd31f9725ac9
Damie <damienmarble@gmail.com>
2019-11-13 21:08:49 -0500

    Cleanup imports.

--- src/main/java/io/github/thisisnozaku/charactercreator/config/WebSecurityConfig.java
+++ src/main/java/io/github/thisisnozaku/charactercreator/config/WebSecurityConfig.java
@@ -1,24 +1,17 @@
 package io.github.thisisnozaku.charactercreator.config;
 
 import io.github.thisisnozaku.charactercreator.authentication.TokenAuthenticationChainFilter;
-import io.github.thisisnozaku.charactercreator.authentication.google.GoogleAuthTokenAuthenticationProvider;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
 import org.springframework.http.HttpHeaders;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.web.authentication.AnonymousAuthenticationFilter;
-import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 import org.springframework.security.web.csrf.CsrfTokenRepository;
 import org.springframework.security.web.csrf.HttpSessionCsrfTokenRepository;
 import org.springframework.web.cors.CorsConfiguration;
@@ -27,7 +20,6 @@ import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import javax.inject.Inject;
 import java.util.Arrays;
-import java.util.List;
 
 /**
  * Created by Damien on 1/30/2016.
